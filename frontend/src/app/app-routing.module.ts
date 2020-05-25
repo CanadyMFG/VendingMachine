b44bd689d460b75239190086d077a900029aeaef
Tristan Hull <tristan@kenpokicks.com>
2020-05-25 14:08:49 -0700

    angular routing & bootstrap

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { EntityComponent } from './entity/entity.component';
+import { EntityListComponent } from './entity-list/entity-list.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path: "home", component: CategoriesComponent},
+  {path: "entity", component: EntityComponent},
+  {path: "entity-list", component: EntityListComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
